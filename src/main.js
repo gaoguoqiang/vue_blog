99a8eab0d7e453e3ea444095689f633f13ea58bd
gaoguoqiang <1289984171@qq.com>
2017-01-06 22:06:26 +0800

    依赖包安装

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import nav from './components/nav.vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import App from './App.vue';
+import nav from './components/nav.vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const index = {}
+const index = {};
 var vm = new Vue({
 	el: '#app',
 	// render: h => h(App)
